29653a6f8b063f6824a65acfbeaeacf7f07d3d7f
Henry <liaohengyi@outlook.com>
2020-07-03 22:18:31 -0400

    flow row

--- src/assigns/task/VFlowDetail.tsx
+++ src/assigns/task/VFlowDetail.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import { VTask } from './VTask';
-import { EasyTime } from 'tonva';
+import { EasyTime, FA } from 'tonva';
 import { stateText, actText } from 'tapp';
 
 export class VFlowDetail extends VTask {
-	header() {return '执行过程'}
+	header() {return '任务过程'}
 
 	protected renderFrom():JSX.Element {return;}
 
 	protected renderMore() {
 		return <>
-			<div className="d-flex mx-3 mt-3 mb-1 align-items-center">{this.renderUser(this.task.worker)}</div>
+			<div className="d-flex mx-3 mt-3 mb-1 align-items-center">
+				{this.renderUser(this.task.worker)}
+			</div>
 			{this.renderFlows()}
 		</>;
 	}
 
 	private renderFlows() {
-		let {flows} = this.task;
+		let {flows, worker} = this.task;
 		return flows.map((v, index) => {
 			let {date, user, state, act} = v;
 			let {me} = stateText(state);
 			let action = actText(act);
 			return <div key={index} className="px-3 py-2 d-flex align-items-center">
-				<span className="mr-3"><EasyTime date={date} always={true} /></span>
-				{!this.isMe(user) && <>{this.renderUser(user)}<span className="mr-3"/></>}
-				{action && <span className="mr-3">{action}</span>}
-				<span className="mr-3">{me}</span>
+				<span className="mr-3 w-8c text-warning">
+					<FA name="angle-double-right" className="mr-1" fixWidth={true} />
+					<span className="small text-muted"><EasyTime date={date} always={true} /></span>
+				</span>
+				<span className="mr-3 text-success">{me}</span>
+				{user !== worker && <>
+					{this.renderUser(user)}<span className="mr-3"/>
+					{action && <span className="mr-3 text-info">{action}</span>}
+				</>}
 			</div>;
 		});
 	}
